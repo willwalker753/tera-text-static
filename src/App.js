0af284d934860f163bdd1c1e63043c54da873349
Will Walker <williamwalker753@gmail.com>
2020-08-17 14:22:29 -0500

    all views working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import User from './components/User';
+import Message from './components/Message';
+import Account from './components/Account';
+import Home from './components/Home';
+import Friend from './components/Friend';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/user/message/:contactId">
+          <Message />
+        </Route>
+        <Route exact path="/user/friend">
+          <Friend />
+        </Route>
+        <Route exact path="/user/account">
+          <Account />
+        </Route>
+        <Route exact path="/user">
+          <User />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
